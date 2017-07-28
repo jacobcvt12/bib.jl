c753aec334e5b588a9360b36cf417ea3b383395c
Jacob Carey <JCarey@telligen.com>
2017-07-27 21:59:30 -0400

    Work more on `Parser` class

--- src/parser.jl
+++ src/parser.jl
@@ -1,3 +1,10 @@
+function clear_comments(data::String)
+    res = replace(data, r"(%.*\n)", "")
+    res = replace(res, r"(comment [^\n]*\n)", "")
+
+    return res
+end
+
 mutable struct Parser
     data::String
     token
@@ -7,11 +14,37 @@ mutable struct Parser
     mode
     records::Dict
     line::Int64
-    white
-    nl
-    token_re
+
+    # regular expressions
+    white::Regex
+    nl::Regex
+    token_re::Regex
 end
 
-function Parser(data)
-    return None
+function Parser(data::String)
+    # compile some regexes
+    white = r"[\n|\s]+"
+    nl = r"[\n]"
+    token_re = r"([^\s\"#%'(){}@,=]+|\n|@|\"|{|}|=|,)"
+
+    Parser(data, 
+           1,
+           white,
+           nl,
+           token_re)
+end
+
+function tokenize(bib::Parser)
+end
+
+function parser(bib::Parser)
+    for item in matchall(token_re, data)
+    for item in self.token_re.finditer(self.data):
+        i = item.group(0)
+        if self.white.match(i) :
+            if self.nl.match(i) :
+                self.line += 1
+            continue
+        else :
+            yield i            
 end
